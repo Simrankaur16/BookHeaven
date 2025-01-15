6eef9dbe4436db51f0ec8fb9d0558d5c853114c8
Simrankaur16 <kau16156@sheridancollege.ca>
2025-01-15 17:44:39 -0500

    Update connection.js

--- backend/conn/connection.js
+++ backend/conn/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const conn = async() =>{
     try {
-        await mongoose.connect("--------");
+        await mongoose.connect("---mongodab connection link here --------");
         console.log("Connected to DB");
     } catch(error){
         console.log(error);
